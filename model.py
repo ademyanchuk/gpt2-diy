dff12fb91cb195c96251220096628e873a590126
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2025-04-19 12:24:20 +0200

    Maybe finish implementing gpt-2 module

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 import math
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 @dataclass
 class GPT2Config:
@@ -15,18 +16,19 @@ class GPT2Config:
 
 class Attention(nn.Module):
   """Scaled Self Attention"""
-  def __init__(self, n_embd, n_heads) -> None:
+  def __init__(self, config: GPT2Config) -> None:
     super().__init__()
-    assert n_embd % n_heads == 0, print(f"{n_embd=} % {n_heads=} must == 0!")
-    self.n_heads = n_heads
+    assert config.n_embd % config.n_heads == 0, print(f"{config.n_embd=} % {config.n_heads=} must == 0!")
+    self.n_heads = config.n_heads
     # one set of weights which includes query, key, value weights
     # n_embd = head_size * n_heads
-    self.c_attn = nn.Linear(n_embd, n_embd * 3) # explicitly (H, HS*NH*3)
-    self.c_proj = nn.Linear(n_embd, n_embd)
+    self.c_attn = nn.Linear(config.n_embd, config.n_embd * 3) # explicitly (H, HS*NH*3)
+    self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+    self.register_buffer('bias', torch.tril(torch.ones((config.block_size, config.block_size))))
   
   def forward(self, x):
     B,T,H = x.shape
-    Hs = H // self.n_heads # head size
+    Hs = H // self.n_heads # head size [dk in the Attention is all you need paper]
     # set of queries, keys and values packed together
     qkv = self.c_attn(x) # -> (B,T,H*3)
     # split them
@@ -39,23 +41,49 @@ class Attention(nn.Module):
     k = k.view((B,T,self.n_heads,Hs)).transpose(1, 2) # (B,Nh,T,Hs)
     v = v.view((B,T,self.n_heads,Hs)).transpose(1, 2) # (B,Nh,T,Hs)
     # compute tokens compatibility (affinity)
+    # (B,Nh,T,Hs) @ (B,Nh,Hs,T)
     att = q @ k.transpose(-2, -1) # -> (B,Nh,T,T)
-    att.div_(math.sqrt(Hs)) # TODO
+    att.div_(math.sqrt(Hs)) # scale by sqrt(Hs)
+    # prevent communication with future tokens, -inf -> 0 after softmax
+    att.masked_fill_(self.bias == 0.0, -torch.inf)
+    # convert affinities into weights
+    att = F.softmax(att, dim=-1) # -> (B,Nh,T,T)
+    # compute weighted sum of values
+    # (B,Nh,T,T) @ (B,Nh,T,Hs)
+    out = att @ v # -> (B,Nh,T,Hs)
+    # transpose back and concat
+    # (B,Nh,T,Hs) -> (B,T,Nh,Hs) -> (B,T,H)
+    out = out.transpose(1, 2).reshape((B,T,H))
+    # project through linear
+    return self.c_proj(out) # -> (B,T,H)
+
+class MLP(nn.Module):
+  def __init__(self, n_embd):
+    super().__init__()
+    self.c_fc = nn.Linear(n_embd, n_embd*4)
+    self.c_proj = nn.Linear(n_embd*4, n_embd)
+    self.act = nn.GELU(approximate="tanh") # as in open ai original
+
+  def forward(self, x):
+    # x shape (B,T,H)
+    x = self.act(self.c_fc(x)) # -> (B,T,H*4)
+    return self.c_proj(x) # -> (B,T,H)
 
 
 class Block(nn.Module):
-  def __init__(self, n_embd) -> None:
+  def __init__(self, config: GPT2Config) -> None:
     super().__init__()
-    self.ln_1 = nn.LayerNorm(n_embd)
-    self.attn = Attention(n_embd)
-    self.ln_2 = nn.LayerNorm(n_embd)
-    self.mlp = MLP(n_embd)
+    self.ln_1 = nn.LayerNorm(config.n_embd)
+    self.attn = Attention(config)
+    self.ln_2 = nn.LayerNorm(config.n_embd)
+    self.mlp = MLP(config.n_embd)
   
   def forward(self, x):
     # x shape (B,T,H)
     # layer norm applied to the input of each sub-block
     x = x + self.attn(self.ln_1(x)) # -> (B,T,H)
     x = x + self.mlp(self.ln_2(x)) # -> (B,T,H)
+    return x
 
 class GPT2(nn.Module):
   def __init__(self, config: GPT2Config):
@@ -63,7 +91,7 @@ class GPT2(nn.Module):
     self.transformer = nn.ModuleDict({
       'wte': nn.Embedding(config.vocab_size, config.n_embd), # (V, H)
       'wpe': nn.Embedding(config.block_size, config.n_embd), # (T, H)
-      'h': nn.ModuleList([Block(config.n_embd) for _ in range(config.n_layers)]), # (H, H)
+      'h': nn.ModuleList([Block(config) for _ in range(config.n_layers)]), # (H, H)
       'ln_f': nn.LayerNorm(config.n_embd) # (H, H)
     })
     self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False) # (H, V)
@@ -79,3 +107,15 @@ class GPT2(nn.Module):
       x = self.transformer['ln_f'](x)  # -> (B,T,H)
       logits = self.lm_head(x)  # -> (B,T,V)
       return logits
+
+if __name__ == "__main__":
+  config = GPT2Config()
+  model = GPT2(config)
+  model.to('cuda')
+  model.eval()
+  print(model)
+  x = torch.randint(config.vocab_size, size=(4, config.block_size)).type(torch.LongTensor)
+  x = x.to('cuda')
+  print(x.shape, x.dtype, x.device)
+  out = model(x)
+  print(out.shape, out.dtype, out.device)
